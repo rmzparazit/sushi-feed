83915fbb7f63bba4874f6045ee7889f5f889daf8
rmzparazit <alinkasget@gmail.com>
2025-09-05 17:55:47 +0300

    Update sushi.py

--- feed/sushi.py
+++ feed/sushi.py
@@ -9,7 +9,7 @@ import re
 import urllib.parse
 
 # --- НАСТРОЙКИ ---
-BASE_URL = "https://суши-стрит.рф/nabory/"
+BASE_URL = "."
 BASE_DOMAIN = "https://суши-стрит.рф"
 OUTPUT_DIR = "output_sushi_street"
 YML_FILE = os.path.join(OUTPUT_DIR, "sushi_street_catalog.xml")
